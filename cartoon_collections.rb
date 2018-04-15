de77afaddb0c1eb13ad4eea3b69230196ed4af1a
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-15 21:11:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,10 @@ end
 
 def long_planeteer_calls(calls)
 calls.each do |call|
-  if call.length < 4
-    return false
-  else
+  if call.length > 4
     return true
+  else
+    return false
 end
 end
 end
